b486afa37d2b62abfc3dfeaa8436aa038d42fa48
Clinic2022 <clinic2022@cicero.cs.hmc.edu>
2023-02-01 16:15:29 -0800

    tested validity of some polygons

--- importCells.sql
+++ importCells.sql
@@ -1,28 +1,43 @@
 -- just import the data as one giant column
+DECLARE @json VARCHAR(MAX)
+
 SELECT @json = BulkColumn
-  FROM OPENROWSET (BULK '/Users/clinic2022/Desktop/Assignment5/data_release_baysor_merfish_gut/data_analysis/baysor/segmentation/poly_per_z.json', SINGLE_CLOB) as j
+  FROM OPENROWSET (BULK '/var/data/data_analysis/baysor/segmentation/poly_per_z.json', SINGLE_CLOB) as j
 
-CREATE DATABASE MouseIleum
-USE DATABASE MouseIleum
-
+-- SELECT * FROM OPENJSON(@json) 
 -- get appropriate columns from this
-select z_id as z, g.id as cell, c.x, c.y
-from @json
-CROSS APPLY OPENJSON(BulkColumn)
+SELECT z_id as z, g.id as cell, c.x, c.y FROM OPENJSON(@json)
+CROSS APPLY OPENJSON(@json)
 WITH(
-    z_id small int, 
+    z_id smallint, 
     geometries NVARCHAR(MAX) AS JSON,
     type nvarchar(18)
   ) as poly
  CROSS apply OPENJSON(poly.geometries) 
  WITH (
     coordinates nvarchar(MAX) AS JSON,
-    type varchar(7) 
+    type varchar(9)
+    -- id int NOT NULL
   ) as g
-  CROSS APPLY OPENJSON(coordinates)
+  CROSS APPLY OPENJSON(g.coordinates)
   WITH (
     x int,
     y int
   ) as c
 
+SELECT * FROM OPENJSON (@JSON)
+WITH (
+    z_id smallint, 
+    geometries NVARCHAR(MAX) AS JSON,
+    type nvarchar(18)) as poly;
+
 -- TODO then save as a file 
+USE MouseIleum
+CREATE TABLE [dbo].[Cells](
+  z TINYINT,
+  cell SMALLINT,
+  x SMALLINT,
+  y SMALLINT
+);
+
+BULK INSERT Cells FROM '/var/data/baysor_polygons.csv' WITH (FIRSTROW = 2, FIELDTERMINATOR = ',');
---
+++ polygonProcessing.sql
@@ -0,0 +1,27 @@
+
+SELECT *
+    INTO NewTable
+    FROM [MouseIleum].[dbo].[Cells]
+    WHERE z = 1 AND cell = 1
+
+SELECT *
+    INTO MoleculesLayer0
+    FROM [MouseIleum].[dbo].[Molecules]
+    WHERE z_pixel = 0
+
+DECLARE @BuildString NVARCHAR(MAX)
+SELECT @BuildString = COALESCE(@BuildString + ',', '') + CAST([x] AS NVARCHAR(50)) + ' ' + CAST([y] AS NVARCHAR(50))
+FROM [MouseIleum].[dbo].[NewTable]
+
+SET @BuildString = 'POLYGON((' + @BuildString + '))';
+DECLARE @PolygonFromPoints geometry = geometry::STPolyFromText(@BuildString, 0);
+SELECT @PolygonFromPoints.STIsValid();
+
+
+SELECT *
+    FROM (
+        SELECT Top(1) *
+        FROM [MouseIleum].[dbo].[MoleculesLayer0]) AS b
+    WHERE @PolygonFromPoints.STIntersects(geometry::Point(b.[x_pixel], b.[y_pixel],0)) = 1
+
+-- Polygon invalidity issue? 
