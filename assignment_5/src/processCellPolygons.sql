70058e94bea24f4a0b37f301ef5625cc0d1105d3
Elizabeth Lucas-Foley <elf.lufo@gmail.com>
2023-02-14 16:01:56 -0800

    small edits

--- assignment_5/src/processCellPolygons.sql
+++ assignment_5/src/processCellPolygons.sql
@@ -3,10 +3,9 @@ USE MouseIleum;
 ALTER TABLE CellPolygons
 ADD polygon geometry;
 
+GO
 UPDATE CellPolygons
     SET polygon = geometry::STGeomFromText(polygon_string, 0);
 
 ALTER TABLE CellPolygons
-    DROP COLUMN polygon_string
-
-SELECT * FROM CellPolygons
+    DROP COLUMN polygon_string;
